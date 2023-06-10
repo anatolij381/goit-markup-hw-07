7404ee985c5e3cc4599d20135491632bd8a0d4a9
anatolij381 <sav4uk.anatolij@gmail.com>
2023-06-10 23:13:26 +0200

    1

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,33 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  let scrollTop = 0;
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    const body = document.querySelector('body');
+    const otherModal = document.querySelector('[data-modal].is-open');
+
+    if (refs.modal.classList.contains('is-hidden')) {
+      if (otherModal) {
+        otherModal.classList.add('is-hidden');
+      }
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      body.style.overflow = 'hidden';
+      body.style.position = 'fixed';
+      body.style.top = `-${scrollTop}px`;
+    } else {
+      body.style.overflow = '';
+      body.style.position = '';
+      body.style.top = '';
+      window.scrollTo(0, scrollTop);
     }
-  })();
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
